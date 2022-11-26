d76eea3bdc6259c9f880b42f4936ebeb800013b8
praveen <praveenarya518@gmail.com>
2022-11-26 12:49:14 +0530

    commit

---
+++ bank_modern_app/src/components/index.js
@@ -0,0 +1,23 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Client from "./Client";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  NavBar,
+  Billing,
+  CardDeal,
+  Business,
+  Client,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
